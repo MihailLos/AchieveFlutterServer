c3cf9e132b3db897436b9ef29ae72a1bb0313dda
ender <enderclub12@gmail.com>
2022-06-09 11:51:05 +0700

    get all user status actives

--- src/main/java/com/example/demo/repository/StatusUserRepository.java
+++ src/main/java/com/example/demo/repository/StatusUserRepository.java
@@ -3,6 +3,10 @@ package com.example.demo.repository;
 import com.example.demo.entity.StatusUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StatusUserRepository extends JpaRepository<StatusUser, Integer> {
     StatusUser findByStatusUser (String name);
+
+    <T> List<T> findBy (Class<T> type);
 }
